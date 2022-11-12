baed1a599fbd00e4a471d06d171fda2cb22bbb4a
Nico Gevers <me@gevious.com>
2022-11-12 11:42:20 -0500

    Showing hands of all players

--- src/components/PrivateCards/index.tsx
+++ src/components/PrivateCards/index.tsx
@@ -1,11 +1,50 @@
-import { Box, Typography } from '@mui/material';
+import { gql, useQuery } from '@apollo/client';
+import { Box, Card, CardContent, Typography } from '@mui/material';
 
+export const GET_YOUR_HAND = gql`
+  query YourHand {
+    cards {
+      score
+      title
+      ability
+    }
+  }
+`;
+
+type PlayingCard = {
+  score: number,
+  title: string,
+  ability: string,
+};
+
+interface CardData {
+  cards: PlayingCard[]
+}
 const PrivateCards = () => {
+  const { loading, error, data } = useQuery<CardData>(GET_YOUR_HAND);
+
+  if (loading) return (<p>Loading ...</p>);
+  if (error) return (<p>Error! </p>);
+
+  const cards = data?.cards!;
+
   return (
-    <Box>
+    <Box aria-label="All cards in your hand">
       <Typography variant="h5">
         Your Hand
       </Typography>
+      {cards.map((c) => (
+	<Card aria-label="Playing Card">
+	  <CardContent>
+	    <Typography variant="h6">
+	      {c.title} - {c.score}
+	    </Typography>
+	    <Typography variant="body2">
+	      {c.ability}
+	    </Typography>
+	  </CardContent>
+	</Card>
+      ))}
     </Box>
   );
 }
--- src/query.mocks.tsx
+++ src/query.mocks.tsx
@@ -1,18 +1,57 @@
 import { GET_GAME_DETAILS } from './components/PublicCards';
+import { GET_YOUR_HAND } from './components/PrivateCards';
+
+const NO_ABILITY: string = "No Ability";
+
+const GameDetails = {
+    disasterType: "Emotional Disaster",
+    disasterDescription: "What Happened",
+    danielScore: 23,
+    hannahScore: 44,
+    daddyScore: 12,
+}
+
+const DanielsHand = [
+  {
+    title: "Di",
+    score: 1,
+    ability: NO_ABILITY,
+  },
+  {
+    title: "Do",
+    score: 3,
+    ability: "strength",
+  }
+];
+const HannahsHand = [
+  {
+    title: "H1",
+    score: 1,
+    ability: NO_ABILITY,
+  },
+  {
+    title: "H2",
+    score: 3,
+    ability: "strength",
+  }
+];
 
 export const mocks: any[] = [{
-  request: {
-    query: GET_GAME_DETAILS,
-  },
-  result: {
-    data: {
-      gameDetails: {
-	  disasterType: "Emotional Disaster",
-	  disasterDescription: "What Happened",
-	  danielScore: 23,
-	  hannahScore: 44,
-	  daddyScore: 12,
-      }
-    }
-  }
+	request: {
+		query: GET_GAME_DETAILS,
+	},
+	result: {
+		data: {
+			gameDetails: GameDetails
+		}
+	}
+}, {
+	request: {
+		query: GET_YOUR_HAND,
+	},
+	result: {
+		data: {
+		  cards: [...DanielsHand, ...HannahsHand]
+		}
+	}
 }];
